0f461b6aa5ec30479333cf806e36cd4a5ad56591
jolojo500 <193266106+jolojo500@users.noreply.github.com>
2025-12-20 11:55:49 -0500

    confirmed metro implementation (not tested tho)

--- stm/stm.filters.ts
+++ stm/stm.filters.ts
@@ -1,9 +1,30 @@
-import type { StmAlert } from "./stm.types.js";
+import type { StmAlert, StmText } from "./stm.types.js";
+
+const METRO_LINES = new Set(["1", "2", "3", "4"]) //confirmé, reserv pour métro chez stm
 
 export function filterMetroAlerts(alerts: StmAlert[]) : StmAlert[]{
     return alerts.filter(alert =>
-        alert.informed_entities.some(e =>
-            e.route_short_name?.toLowerCase().includes("metro")
-        )
+        isMetroAlert(alert) && !isNormalService(alert)
     )
 }
+
+
+
+
+function getFrenchText(texts: StmText[]): string{
+    return texts.find(t=> t.language === "fr")?.text ?? ""
+}
+
+function isMetroAlert(alert: StmAlert): boolean{
+    return alert.informed_entities.some(
+        e => e.route_short_name && METRO_LINES.has(e.route_short_name) //aka exist + actual metro
+    )
+}
+
+function isNormalService(alert: StmAlert): boolean{
+    const text = getFrenchText(alert.description_texts).toLowerCase()
+    return text.includes("Service normal") //unchanged for 2+ years why bother
+}
+
+//etc, par ex we can add interactive choice later which will basically mean more funcs
+
