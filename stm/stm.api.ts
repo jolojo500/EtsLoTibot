d9329cff6e9cd6c64295211459903d222e31f172
jolojo500 <193266106+jolojo500@users.noreply.github.com>
2025-12-20 21:35:50 -0500

    big bruh forgot this

--- stm/stm.api.ts
+++ stm/stm.api.ts
@@ -1,9 +1,13 @@
 import type { StmResponse } from "./stm.types.js"
 
-const STM_ALTERTS_URL = "api.stm.info/pub/od/i3/v2/messages/etatservice"
+const STM_ALTERTS_URL = "https://api.stm.info/pub/od/i3/v2/messages/etatservice"
 
 export async function fetchStmAlerts(): Promise<StmResponse>{
-    const res = await fetch(STM_ALTERTS_URL)
+    const res = await fetch(STM_ALTERTS_URL, {
+        headers: {
+            "apiKey": process.env.STM_API_KEY!
+        }
+    })
     
     if(!res.ok){
         throw new Error(`Stm Api error: ${res.status}`)
