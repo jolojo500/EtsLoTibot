5e2cfbcc08b5d3a5b5920534bac55136244c4775
jolojo500 <193266106+jolojo500@users.noreply.github.com>
2026-02-10 22:30:05 -0500

    wonky fix for auto check, basically fixed timezone stuff (render serv prob in other timezone) and adding caching for no dupes (no need to add to to /stm command handling because ux, user asked user sees dont care if someone else asked same thing). If it works tommorrow peak, embed would be the next nice step

--- antiSleep.ts
+++ antiSleep.ts
@@ -2,6 +2,8 @@ import { type Client, ChannelType } from "discord.js";
 import { fetchStmAlerts } from "./stm/stm.api.js";
 import { filterMetroAlerts } from "./stm/stm.filters.js";
 
+
+const lastAlertsCache = new Map<string, number>(); //"id"(lets say) , timestamp
 export async function autoCheckStm(client: Client){
     if(!isPeakHour()) return
 
@@ -9,12 +11,27 @@ export async function autoCheckStm(client: Client){
     const alerts = filterMetroAlerts(data.alerts)
 
     if(alerts.length === 0) return
+    //filer old stuff, didnt put in filter func cuz their job is categorising tbf this use case too specific
+    const now = Date.now();
+    const newAlerts = alerts.filter(alert => {
+      const key = JSON.stringify(alert.description_texts);
+      const lastSent = lastAlertsCache.get(key);
+
+      if(!lastSent || now - lastSent > 30 * 60 * 1000){ //if never been sent or older than 30mins
+        lastAlertsCache.set(key, now);
+        return true;
+      }
+
+      return false;
+    });
+    if(newAlerts.length === 0) return;
+    //TODO gotta make map cleaning sometimes perhaps and maybe add better handling lol even if we dont got an actual id maybe hashmap or simply lowercase idk
 
     const channel = await client.channels.fetch(process.env.CHANNEL_ID!)
     if(channel?.type !== ChannelType.GuildText) return
 
-   
-    const msg = alerts.map(a =>
+    //was alerts before handling of dupes
+    const msg = newAlerts.map(a =>
         a.description_texts.find(t=> t.language === "fr")?.text
     ).join("\n\n")
 
@@ -23,8 +40,10 @@ export async function autoCheckStm(client: Client){
 
 
 function isPeakHour(date = new Date()) {
-  const h = date.getHours();
-  const m = date.getMinutes();
+  const actualLocalTime = new Date(date.toLocaleString("ens-US",{ timeZone: "America/Montreal"}));
+
+  const h = actualLocalTime.getHours();
+  const m = actualLocalTime.getMinutes();
   const time = h * 60 + m;
 
   return (
